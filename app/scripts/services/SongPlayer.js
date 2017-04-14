9bf6958e9e6fc5c2db65df9bda4f3cd8d8945281
Harold Asai <koichiroasai@gmail.com>
2017-04-13 22:24:22 -0700

    blocjams-angular-assignment7-completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -13,8 +13,7 @@
          * @function setSong
          * @desc Stops currently playing song and loads new audio file as currentBuzzObject
          * @param {Object} song
-         */
-         
+         */         
          var setSong = function(song) {
              if (currentBuzzObject) {
                  currentBuzzObject.stop();
@@ -29,11 +28,29 @@
              currentSong = song;
          };
          
+         /**
+         * @function playSong
+         * @desc Play audio file stored in currentBuzzObject and set playing attribute to true
+         * @param {Object} song
+         */          
+         var playSong = function(song) {
+             currentBuzzObject.play();
+             song.playing = true;
+         };
+         
+         /**
+         * @function play
+         * @desc set new song and play if current song is not equal to the new song. Play current audio file if current song is equal to new song and the audio file is paused.
+         * @param {Object} song
+         */  
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
                  setSong(song);
+                 playSong(song);
+                 /*
                  currentBuzzObject.play();
                  song.playing = true;
+                 */
              } else if (currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
                      currentBuzzObject.play();
@@ -41,6 +58,11 @@
              }
          };
          
+         /**
+         * @function pause
+         * @desc pause current audio file and set playing attribute to false.
+         * @param {Object} song
+         */  
          SongPlayer.pause = function(song) {
              currentBuzzObject.pause();
              song.playing = false;
