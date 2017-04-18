6f215d23d43116361a8242b199569fb90eaf7f62
Harold Asai <koichiroasai@gmail.com>
2017-04-18 11:13:52 -0700

    blocjams-angular-features-autoplay-completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,21 +1,21 @@
  (function() {
      function SongPlayer($rootScope, Fixtures) {
          var SongPlayer = {};
-         
+
          var currentAlbum = Fixtures.getAlbum();
          /**
          * @desc Buzz object audio file
          * @type {Object}
          */
          var currentBuzzObject = null;
-         
+
          /**
          * @function setSong
          * @desc Stops currently playing song and loads new audio file as currentBuzzObject
          * @param {Object} song
-         */         
+         */
          var setSong = function(song) {
-             if (currentBuzzObject) {              
+             if (currentBuzzObject) {
                  stopSong(SongPlayer.currentSong);
                  /*
                  currentBuzzObject.stop();
@@ -27,66 +27,77 @@
                  formats: ['mp3'],
                  preload: true
              });
-             
+
              currentBuzzObject.bind('timeupdate', function() {
+
+                 //call autoPlay() if auto-play slider is on
+                 if(SongPlayer.autoPlayOn) {
+                     if(this.isEnded()) {
+                         SongPlayer.autoPlay();
+                     }
+                 }
+
+                 //update currentTIme
                  $rootScope.$apply(function() {
                      SongPlayer.currentTime = currentBuzzObject.getTime();
                  });
              });
-             
+
              currentBuzzObject.bind('volumechange', function() {
                  $rootScope.$apply(function() {
                      SongPlayer.volume = currentBuzzObject.getVolume();
                  });
              });
-             
+
 
              SongPlayer.currentSong = song;
          };
-         
+
          /**
          * @function playSong
          * @desc Play audio file stored in currentBuzzObject and set playing attribute to true
          * @param {Object} song
-         */          
+         */
          var playSong = function(song) {
              currentBuzzObject.play();
              song.playing = true;
          };
-         
+
          var stopSong = function(song) {
              currentBuzzObject.stop();
              song.playing = null;
          }
-         
-         
+
+
          var getSongIndex = function(song) {
               return currentAlbum.songs.indexOf(song);
          };
-         
+
          /**
          * @desc Active song object from list of songs
          * @type {Object}
          */
          SongPlayer.currentSong = null;
-         
+
          /**
          * @desc Current playback time (in seconds) of currently playing song
          * @type {Number}
          */
          SongPlayer.currentTime = null;
-         
+
          /**
          * @desc volume (in percentile) of currently playing song
          * @type {Number}
          */
          SongPlayer.volume = null;
-         
+
+         SongPlayer.autoPlayOn = false;
+
          /**
          * @function play
          * @desc set new song and play if current song is not equal to the new song. Play current audio file if current song is equal to new song and the audio file is paused.
          * @param {Object} song
-         */  
+         */
          SongPlayer.play = function(song) {
              song = song || SongPlayer.currentSong;
              if (SongPlayer.currentSong !== song) {
@@ -106,18 +117,18 @@
                  }
              }
          };
-         
+
          /**
          * @function pause
          * @desc pause current audio file and set playing attribute to false.
          * @param {Object} song
-         */  
+         */
          SongPlayer.pause = function(song) {
              song = song || SongPlayer.currentSong;
              currentBuzzObject.pause();
              song.playing = false;
          };
-         
+
          /**
          * @function next
          * @find next song and play it.
@@ -125,7 +136,7 @@
          SongPlayer.next = function() {
              var currentSongIndex = getSongIndex(SongPlayer.currentSong);
              currentSongIndex++;
-             
+
              if (currentSongIndex >= Fixtures.getAlbum().songs.length) {
                  stopSong(SongPlayer.currentSong);
                  /*
@@ -138,7 +149,7 @@
                  playSong(song);
              }
          };
-         
+
          /**
          * @function previous
          * @find previous song and play it.
@@ -146,7 +157,7 @@
          SongPlayer.previous = function() {
              var currentSongIndex = getSongIndex(SongPlayer.currentSong);
              currentSongIndex--;
-             
+
              if (currentSongIndex < 0) {
                  stopSong(SongPlayer.currentSong);
                  /*
@@ -159,7 +170,22 @@
                  playSong(song);
              }
          };
-         
+
+         /**
+         * @function autoPlay
+         * @desc autoplay next song after current song has ended
+         */
+         SongPlayer.autoPlay = function() {
+             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+             currentSongIndex++;
+
+             if (currentSongIndex < Fixtures.getAlbum().songs.length) {
+                 var song = currentAlbum.songs[currentSongIndex];
+                 setSong(song);
+                 playSong(song);
+             }
+         };
+
          /**
          * @function setCurrentTime
          * @desc Set current time (in seconds) of currently playing song
@@ -170,16 +196,16 @@
                  currentBuzzObject.setTime(time);
              }
          };
-         
+
          SongPlayer.setVolume = function(volume) {
              if (currentBuzzObject) {
                  currentBuzzObject.setVolume(volume);
              }
          };
-         
+
          return SongPlayer;
      }
- 
+
      angular
          .module('blocJams')
          .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
